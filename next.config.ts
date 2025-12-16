23f37e8aa86202fd6a5b3e37e8ceb9092338d693
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-12-16 15:53:07 +0700

    fix(ci): cicd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Cần thiết cho static export
+  },
 };
 
 export default nextConfig;
